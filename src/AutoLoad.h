b02b6670998345dbef953bd0f25b2c2cac5c8da5
Moritz Geilinger <moritzge@inf.ethz.ch>
2018-03-13 23:01:58 +0100

    Added AutoGen include dir to g++ command
    Still TODO for cmake and clang!

--- src/AutoLoad.h
+++ src/AutoLoad.h
@@ -52,7 +52,7 @@ bool buildLibrary(const std::string &code, const std::string &libName, std::stri
 	std::string compile_cmd = "clang++ -fPIC -shared -o "+libName+"/lib"+libName+".so "+libName+"/"+libName+".cpp";
 #elif defined(__GNUC__) || defined(__GNUG__)
 	std::cout << "Using `g++` to compile '" << libName << "'." << std::endl;
-	std::string compile_cmd = "g++ -fPIC -shared -o "+libName+"/lib"+libName+".so "+libName+"/"+libName+".cpp";
+	std::string compile_cmd = "g++ -fPIC -I" AUTOGEN_SRC_DIR " -shared -o "+libName+"/lib"+libName+".so "+libName+"/"+libName+".cpp";
 #else
 	std::cout << "Using `cmake --build` to compile '" << libName << "'." << std::endl;
 	// create CMakeLists.txt
